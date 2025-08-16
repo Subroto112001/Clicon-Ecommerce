e6282be15a7a2cdd97bf29a70b4e1bd3c183e52c
Subroto112001 <subrotokumarbarman@gmail.com>
2025-08-16 16:13:41 +0600

    update

--- Font-end/src/Component/CoomonComponent/SwiperSlider/SwiperSlider.tsx
+++ Font-end/src/Component/CoomonComponent/SwiperSlider/SwiperSlider.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Pagination, EffectFlip, A11y,Autoplay, EffectCards } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 // @ts-ignore
 import "swiper/css";
@@ -9,33 +9,35 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 
-
-import { assets } from '../../../Helpers/ImageProvider';
-const SwiperSlider = () => {
+interface propsType {
+  children: React.ReactNode;
+  animationStyle: string;
+  ispagination: boolean;
+}
+const SwiperSlider: React.FC<propsType> = ({
+  children,
+  animationStyle = "flip",
+  ispagination ,
+}) => {
   return (
     <div>
       <Swiper
-        modules={[Navigation, Pagination,  A11y]}
+        modules={[...(ispagination? [Pagination] : []), A11y, EffectFlip, Autoplay, EffectCards]}
+        effect={animationStyle}
         spaceBetween={50}
         slidesPerView={1}
-        navigation
+        autoplay={{ delay: 4500, disableOnInteraction: false }}
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log("slide change")}
       >
         {[1, 2, 3, 4].map((item) => (
-          <SwiperSlide>
-            <img
-              src={assets.BannerLeft}
-              alt={assets.BannerLeft}
-              className="w-full h-full object-cover rounded"
-            />
-          </SwiperSlide>
+          <SwiperSlide>{children}</SwiperSlide>
         ))}
       </Swiper>
     </div>
   );
-}
+};
 
 export default React.memo(SwiperSlider);
--- Font-end/src/Component/HomeCoomponent/Banner/Index.tsx
+++ Font-end/src/Component/HomeCoomponent/Banner/Index.tsx
@@ -11,24 +11,33 @@ const Banner = () => {
       <div className="!py-10">
         <Containere>
           <div className="grid grid-cols-[2fr_1fr] gap-5">
-            <div className="rounded w-[1000px]">
-              
-              <SwiperSlider/>
+            <div className="rounded w-[900px] h-[540px]">
+              <SwiperSlider animationStyle="flip" ispagination={true}>
+                <img
+                  src={assets.BannerLeft}
+                  alt={assets.BannerLeft}
+                  className="w-full h-full object-cover rounded cursor-grab"
+                />
+              </SwiperSlider>
             </div>
             <div className="grid grid-rows-2 justify-items-stretch gap-y-5 ">
-              <div className=" rounded ">
-                <img
-                  src={assets.BannerRight1}
-                  alt={assets.BannerRight1}
-                  className="w-full h-full object-cover rounded"
-                />
+              <div className=" rounded w-[400px] h-[300px] ">
+                <SwiperSlider animationStyle="cards" ispagination={true}>
+                  <img
+                    src={assets.BannerRight1}
+                    alt={assets.BannerRight1}
+                    className="w-full h-full object-cover rounded"
+                  />
+                </SwiperSlider>
               </div>
-              <div className=" rounded">
-                <img
-                  src={assets.BannerRight2}
-                  alt={assets.BannerRight2}
-                  className="w-full h-full object-cover rounded"
-                />
+              <div className=" rounded w-[400px] h-[300px]">
+                <SwiperSlider animationStyle="cards" ispagination={true}>
+                  <img
+                    src={assets.BannerRight2}
+                    alt={assets.BannerRight2}
+                    className="w-full h-full object-cover rounded"
+                  />
+                </SwiperSlider>
               </div>
             </div>
           </div>
