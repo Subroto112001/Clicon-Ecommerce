4b2f19eb46f071d58c2c05713653532adaf38b64
Subroto112001 <subrotokumarbarman@gmail.com>
2025-09-16 16:53:08 +0600

    added every component shop page

--- Font-end/src/Component/ShopComponent/Leftside/Index.tsx
+++ Font-end/src/Component/ShopComponent/Leftside/Index.tsx
@@ -5,8 +5,21 @@ import { useApp } from "../../../Hooks/Context/Contextapi";
 import { GetfeaturesProduct } from "../../../Api/featuresProduct";
 import { useQuery } from "@tanstack/react-query";
 const RightSideOfShopComponent = () => {
- const [products, setProducts] = useState<any[]>([]);
-
+  const [products, setProducts] = useState<any[]>([]);
+  const [randomTagsColor, setRandomTagsColor] = useState<string[]>([
+    "primary-400",
+    "primary-500",
+    "gray-400",
+    "gray-500",
+    "secondary-400",
+    "secondary-500",
+    "tertiary-400",
+    "tertiary-500",
+    "success-400",
+    "success-500",
+    "warning-400",
+    "warning-500",
+  ]);
 
   const { category, fetchProductByCategory, fetchPosts } = useApp();
   const [value, setValue] = useState<[number, number]>([5, 85]);
@@ -19,18 +32,16 @@ const RightSideOfShopComponent = () => {
     }
   };
   // Query for all products
-  const {
-    data,
-  } = useQuery({
+  const { data } = useQuery({
     queryKey: ["featured-products"],
     queryFn: GetfeaturesProduct,
   });
 
   useEffect(() => {
-  setProducts(data?.products);
-}, [data])
+    setProducts(data?.products);
+  }, [data]);
   console.log(products);
-  
+
   const priceOptions = [
     "All Price",
     "Under $20",
@@ -41,7 +52,6 @@ const RightSideOfShopComponent = () => {
     "$1,000 to $10,000",
   ];
 
-  
   const [selectedBrands, setSelectedBrands] = useState<string[]>([]);
 
   const handleBrandChange = (brandId: string) => {
@@ -97,16 +107,18 @@ const RightSideOfShopComponent = () => {
         </div>
       </div>
 
-      <div className=" border-b border-gray-300 pb-10 flex flex-col gap-3">
-        <h3 className="label2 text-gray-900">PRICE RANGE</h3>
-        <div className="mt-[23px] felx flex-col ">
-          <RangeSlider
-            id="range-slider-yellow"
-            value={value}
-            onInput={setValue}
-          />
+      <div className="  pb-10 flex flex-col gap-3">
+        <div className=" felx flex-col  ">
+          <h3 className="label2 text-gray-900">PRICE RANGE</h3>
+          <div className="mt-[23px]">
+            <RangeSlider
+              id="range-slider-yellow"
+              value={value}
+              onInput={setValue}
+            />
+          </div>
 
-          <div className="flex mt-6 flex-row justify-between items-center font-normal font-Public text-[14px] text-gray-500">
+          <div className="flex mt-6  flex-row justify-between items-center font-normal font-Public text-[14px] text-gray-500">
             <button className="w-[120px] h-[40px] rounded border border-gray-100 cursor-pointer hover:border-gray-200 transition-all duration-300">
               MIN PRICE
             </button>
@@ -114,62 +126,83 @@ const RightSideOfShopComponent = () => {
               MAX PRICE
             </button>
           </div>
-          <div className="flex flex-col gap-4 mt-4">
-            {priceOptions.map((option, index) => (
-              <label
-                key={index}
-                className="flex cursor-pointer items-center gap-3"
-              >
-                {/* The actual radio button is visually hidden but accessible */}
-                <input
-                  type="radio"
-                  name="price-option"
-                  value={option}
-                  checked={selectedOption === option}
-                  onChange={() => setSelectedOption(option)}
-                  className="peer sr-only"
-                />
+        </div>
+        <div className="flex flex-col gap-4   py-6 border-b border-gray-300">
+          {priceOptions.map((option, index) => (
+            <label
+              key={index}
+              className="flex cursor-pointer items-center gap-3"
+            >
+              {/* The actual radio button is visually hidden but accessible */}
+              <input
+                type="radio"
+                name="price-option"
+                value={option}
+                checked={selectedOption === option}
+                onChange={() => setSelectedOption(option)}
+                className="peer sr-only"
+              />
 
-                {/* The custom-styled radio button */}
-                <div
-                  className="
+              {/* The custom-styled radio button */}
+              <div
+                className="
                 h-5 w-5 rounded-full border-2 
                 border-gray-300 
                 transition-all 
                 peer-checked:border-orange-500
               "
-                ></div>
+              ></div>
 
-                {/* The text label */}
-                <span className="text-gray-700 peer-checked:text-gray-900">
-                  {option}
-                </span>
+              {/* The text label */}
+              <span className="text-gray-700 peer-checked:text-gray-900">
+                {option}
+              </span>
+            </label>
+          ))}
+        </div>
+        <div className=" py-7 border-b border-gray-300 flex flex-col gap-3">
+          <h3 className="label2 text-gray-900">POPULAR BRANDS</h3>
+          <div className="flex flex-col gap-3">
+            {products?.slice(0, 14).map((item) => (
+              <label
+                key={item.id}
+                className="flex items-center space-x-2 cursor-pointer "
+              >
+                <input
+                  type="checkbox"
+                  checked={selectedBrands.includes(item.id)}
+                  onChange={() => handleBrandChange(item.id)}
+                  className="form-checkbox h-5 w-5 rounded-md border-gray-300 "
+                />
+                <span className="text-gray-800">{item.brand}</span>
               </label>
             ))}
           </div>
-          <div className=" mt-7 rounded-lg flex flex-col gap-3">
-            <h3 className="label2 text-gray-900">POPULAR BRANDS</h3>
-            <div className="flex flex-col gap-3">
-              {products?.slice(0, 14).map((item) => (
-                <label
-                  key={item.id}
-                  className="flex items-center space-x-2 cursor-pointer"
-                >
-                  <input
-                    type="checkbox"
-                    checked={selectedBrands.includes(item.id)}
-                    onChange={() => handleBrandChange(item.id)}
-                    className="form-checkbox text-orange-500 h-5 w-5 rounded-md border-gray-300 focus:ring-orange-500"
-                  />
-                  <span className="text-gray-800">{item.brand}</span>
-                </label>
-              ))}
-            </div>
+        </div>
+
+        <div className=" py-7  border-b border-gray-300  flex flex-col gap-3">
+          <h3 className="label2 text-gray-900">POPULAR TAGS</h3>
+          <div className="grid grid-cols-2 gap-3">
+            {products?.slice(0, 14).map((item) => (
+              <span
+                className={`text-gray-800 text-center items-center cursor-pointer px-1 py-1 justify-center bg-${
+                  randomTagsColor[
+                    Math.floor(Math.random() * randomTagsColor.length)
+                  ]
+                } border border-${
+                  randomTagsColor[
+                    Math.floor(Math.random() * randomTagsColor.length)
+                  ]
+                } inline-block hover:bg-gray-100 transition-all duration-300 rounded`}
+              >
+                {item.brand.split(" ")[0]}
+              </span>
+            ))}
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default RightSideOfShopComponent;
